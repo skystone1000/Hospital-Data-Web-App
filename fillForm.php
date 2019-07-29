55e731d2e7590cf630d8de5b8776c11d3adea0bd
Aditya Mahajan <34326438+skystone1000@users.noreply.github.com>
2019-07-29 21:40:02 +0530

    Adding Basic GUI for the web app
    Added the fill form and the index page.

---
+++ fillForm.php
@@ -0,0 +1,201 @@
+<?php
+    include './headFoot/header.php';
+?>
+
+    <div class="container">
+        <h1> Patient Details<br>
+        <small>Mahajan Homeo Clinic</small>
+        </h1>
+    </div>
+
+    <form action="action.php" method="get" class="container">
+        <div class="form-row">
+            <div class="col-md-4 mb-3">
+                <label>First name</label>
+                <input type="text" class="form-control" name="firstName" placeholder="First name" required>
+            </div>
+            <div class="col-md-4 mb-3">
+                <label>Middle name</label>
+                <input type="text" class="form-control" name="middleName" placeholder="Middle name"  >
+            </div>
+            <div class="col-md-4 mb-3">
+                <label>Last name</label>
+                <input type="text" class="form-control" name="lastName" placeholder="Last name" required>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="col-md-4 mb-3">
+                <label>Age</label>
+                <input type="text" class="form-control" name="age" placeholder="Age" required>
+            </div>
+            <div class="col-md-4 mb-3">
+                <label>Sex</label>
+                <input type="text" class="form-control" name="sex" placeholder="Gender"  required>
+            </div>
+            <div class="col-md-4 mb-3">
+                <label>Occupation</label>
+                <input type="text" class="form-control" name="occupation" placeholder="Occupation">
+            </div>
+        </div> 
+        <br>
+
+        <div class="form-group">
+                <label>Address : </label>
+                <input type="text" class="form-control input-lg" name="address" placeholder="Address" required>
+        </div> <br>
+
+        <div class="form-row">
+            <div class="col-md-4 mb-3">
+                <label>Phone Number : </label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone">
+            </div> 
+            <div class="col-md-4 mb-3">
+                <label>Registration Number : </label>
+                <input type="text" class="form-control" id="regno" name="regno" placeholder="Registration Number">
+            </div> 
+        </div>
+
+        <br>
+        <h3>Chief Complaints</h3>
+        <div class="form-group">
+            <label>CC 1 : - </label>
+            <input type="text" class="form-control" name="cc1" placeholder="Complaint 1">
+        </div>
+        
+        <div class="form-group">
+            <label>CC 2 : - </label>
+            <input type="text" class="form-control" name="cc2" placeholder="Complaint 2">
+        </div>
+        
+        <div class="form-group">
+            <label>CC 3 : - </label>
+            <input type="text" class="form-control" name="cc3" placeholder="Complaint 3">
+        </div>
+
+        <div class="form-group">
+            <label>Appetite : - </label>
+            <textarea class="form-control" rows="2" name="appetite"  placeholder="How's the Appetite of the patient?"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Desire : - </label>
+            <textarea class="form-control" rows="5" name="desire" placeholder="What does the Patient Like?"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Aversions : - </label>
+            <textarea class="form-control" rows="2" name="aversions"  placeholder="What are the Aversions?"></textarea>
+        </div>
+        
+        <div class="form-group">
+            <label>Thirst : - </label>
+            <textarea class="form-control" rows="2" name="thirst" placeholder="How's the Thirst?"></textarea>
+        </div>
+        
+        <div class="form-group">
+            <label>Perspiration : - </label>
+            <textarea class="form-control" rows="2" name="perspiration" placeholder="Perspiration ..."></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Sleep : - </label>
+            <textarea class="form-control" rows="2" name="sleep" placeholder="Do you sleep Well?"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Stool : - </label>
+            <textarea class="form-control" rows="2" name="stool" placeholder="How is the Morning ritual?"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Urine : - </label>
+            <textarea class="form-control" rows="2" name="urine" placeholder="Do you Drink lots of water?"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Menses : - </label>
+            <textarea class="form-control" rows="2" name="menses" placeholder="Menstrual Cycle"></textarea>
+        </div>
+        
+
+        <div class="form-group">
+            <label>Thermal : - </label>
+            <textarea class="form-control" rows="1" name="thermal" placeholder="Body TTemperature conditions"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Mind : - </label>
+            <textarea class="form-control" rows="10" name="mind" placeholder="Most Imp Ques? "></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Hobbies : - </label>
+            <textarea class="form-control" rows="5" name="hobbies" placeholder="What are your hobbies?"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Particulars : - </label>
+            <textarea class="form-control" rows="15" name="particulars" placeholder="Particulars ..."></textarea>
+        </div>
+
+
+        <div class="form-group">
+            <label>On-Examination : - </label>
+            <textarea class="form-control" rows="8" name="on_examination" placeholder="What did you seeeee ...."></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Path-Inv : - </label>
+            <textarea class="form-control" rows="8" name="path_inv" placeholder="Pathelogy Investigation"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Previous-Rx : - </label>
+            <textarea class="form-control" rows="8" name="previous_rx" placeholder="Have you taken some previous Statements"></textarea>
+        </div>
+
+
+        <div class="form-group">
+            <label>Past-history : - </label>
+            <textarea class="form-control" rows="8" name="past_history" placeholder="What's your history?"></textarea>
+        </div>
+
+
+        <div class="form-group">
+            <label>Family-history : - </label>
+            <textarea class="form-control" rows="2" name="family_history" placeholder="Family background history"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Treatment : - </label>
+            <textarea class="form-control" rows="3" name="treatment" placeholder="Treatment Given"></textarea>
+        </div>
+
+
+        <div class="form-group">
+            <label>Follow Up 1 : - </label>
+            <textarea class="form-control" rows="2" name="followUp1" placeholder="Leave Blank if first time"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Follow Up 2 : - </label>
+            <textarea class="form-control" rows="2" name="followUp2" placeholder="Leave Blank if first time"></textarea>
+        </div>
+        
+        <div class="form-group">
+            <label>Follow Up 3 : - </label>
+            <textarea class="form-control" rows="2" name="followUp3" placeholder="Leave Blank if first time"></textarea>
+        </div>
+        
+        <div class="form-group">
+            <label>Follow Up 4 : - </label>
+            <textarea class="form-control" rows="2" name="followUp4" placeholder="Leave Blank if first time"></textarea>
+        </div>
+        
+        <button class="btn btn-primary" type="submit">Submit form</button>
+    </form>
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+	include './headFoot/header.php';
+?>
+
+<br>
+<div class="container d-flex justify-content-center">
+	<h1>Welcome to <b>Mahajan Homeo Clinic</b> Database App</h1>
+</div>
+
+
+<div class="container">
+	<div class="row">
+	    <div class="col">
+			<div class="jumbotron">
+				<h1 class="display-4">Hello, There!</h1>
+				<p class="lead">Ready for a tour, get ready to see all the cases of our Clinic.</p>
+				<hr class="my-4">
+				<p>Click Here to get Started ...</p>
+				<p class="lead">
+					<a class="btn btn-primary btn-lg" href="records.php" role="button">Patient Records</a>
+				</p>
+			</div>
+		</div>
+		<div class="col">
+			<div class="jumbotron">
+				<h1 class="display-4">Ok, Whats next!</h1>
+				<p class="lead">Ready for the next, Want to add some <br>more patients.</p>
+				<hr class="my-4">
+				<p>Click Here to get Started ...</p>
+				<p class="lead">
+					<a class="btn btn-primary btn-lg" href="fillForm.php" role="button">Patient Records</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
